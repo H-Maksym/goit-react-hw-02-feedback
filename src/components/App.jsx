839450793896507db1d279b6137d130da224a88f
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-09-04 00:52:47 +0300

    task1 solved witout style and refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,4 @@
-import Profile from 'components/Profile/Profile';
-import Statistics from 'components/Statistics/Statistics';
-import FriendList from 'components/FriendList/FriendList';
-import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
-
-import user from 'data/user.json';
-import data from 'data/data.json';
-import friends from 'data/friends.json';
-import transaction from 'data/transactions.json';
+import Counter from 'components/Counter/Counter';
 
 export const App = () => {
   return (
@@ -16,27 +8,14 @@ export const App = () => {
         minHeight: '100vh',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
+        // justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
     >
       <h1>React homework</h1>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-
-      <FriendList friends={friends} />
-
-      <TransactionHistory items={transaction} />
+      <Counter />
     </div>
   );
 };
